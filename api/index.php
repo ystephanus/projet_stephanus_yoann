544c66461172f568629e74087fce240cfbc7c45b
yoann STEPHANUS <y.stephanus@laposte.net>
2022-01-27 14:28:57 +0100

    init

---
+++ api/index.php
@@ -0,0 +1,17 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+$app = AppFactory::create();
+
+$app->get('/hello/{name}',
+
+function (Request $resquest, Response $response,$args) {
+
+return $response->getBody()->write(json_encode({\"nom\":\"$args['name']\"}));
+
+$app->run();
+
+?>y
