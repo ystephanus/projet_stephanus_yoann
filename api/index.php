a98fa1948104969052f4bbb73d7e52deefc4990e
yoann STEPHANUS <y.stephanus@laposte.net>
2022-01-27 15:53:16 +0100

    test

--- api/index.php
+++ api/index.php
@@ -3,15 +3,14 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
+
 $app = AppFactory::create();
 
-$app->get('/hello/{name}',
-
-function (Request $resquest, Response $response,$args) {
-
-return $response->getBody()->write(json_encode({\"nom\":\"$args['name']\"}));
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
 
 $app->run();
-
-?>
